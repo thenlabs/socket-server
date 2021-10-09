57da3b93a4bd7b8cc57c2408ec1b99a6c69742cd
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2021-10-09 15:03:57 -0400

    First

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+;
+
+$config = new PhpCsFixer\Config();
+
+return $config->setRules([
+        '@PSR1' => true,
+        '@PSR2' => true,
+        'no_unused_imports' => true,
+        'single_blank_line_before_namespace' => true,
+    ])
+    ->setFinder($finder)
+;
